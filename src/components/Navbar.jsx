9a7025e68df105785ad046b5012bf4e30b5cbe5b
Rajid Ansari <rajidansari25@gmail.com>
2025-04-09 10:25:41 +0530

    btn animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,11 +11,13 @@ const Navbar = () => {
 				</h1>
 			</div>
 			<div id='tabs' className='flex gap-4 md:gap-9 text-gray-100 font-bold '>
+				<button className='resume-btn border-blue-500 border h-10 w-24 rounded-full gradient-border-mask bg-transparent overflow-hidden hover:before:w-24'>
 				<a 
-				className='border-blue-500 border py-2 px-6 rounded-full gradient-border-mask hover:bg-blue-500 transition-all duration-300 ease-linear delay-0 '
+				className=''
 				href="https://ik.imagekit.io/vx7u9slhq/assets/rajid_resume.pdf?updatedAt=1744104665183" target='_blank' rel='noopener noreferrer'
 				>Resume
 				</a>
+				</button>
 
 			</div>
 		</nav>
